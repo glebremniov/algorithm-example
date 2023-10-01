ddc611485a22d91f9da96611b531796678464905
Gleb Remniov <g.remniov@gmail.com>
2023-10-01 23:58:31 +0300

    Naive implementation

--- src/main/java/org/example/Destructor.java
+++ src/main/java/org/example/Destructor.java
@@ -2,9 +2,44 @@ package org.example;
 
 public class Destructor {
 
-
   public int destruct(int n) {
-    return -1;
+    if (n < 1) {
+      return -1;
+    }
+
+    if (n < 10) {
+      return n;
+    }
+
+    int targetNumber = 0;
+    int step;
+
+    while (true) {
+      step = (int) Math.floor(Math.log10(targetNumber + 1)) + 1;
+
+      if (n < step) {
+        break;
+      }
+
+      n -= step;
+      targetNumber++;
+    }
+
+    if (n > 0) {
+      targetNumber++;
+
+      while (n <= --step) {
+        targetNumber /= 10;
+      }
+    }
+
+    int result = targetNumber % 10;
+
+    System.out.println("Target number: " + targetNumber);
+    System.out.println("Target index: " + n);
+    System.out.println("Target digit: " + result);
+
+    return result;
   }
 
 }
--- src/test/java/org/example/DestructorTest.java
+++ src/test/java/org/example/DestructorTest.java
@@ -1,36 +1,32 @@
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class DestructorTest {
 
   private final Destructor subject = new Destructor();
 
   @ParameterizedTest
-  @MethodSource("validArguments")
+  @CsvSource({
+      "1, 1", "2, 2", "3, 3", "4, 4", "5, 5",
+      "6, 6", "7, 7", "8, 8", "9, 9", "10, 1",
+      "11, 0", "12, 1", "13, 1", "14, 1", "15, 2",
+      "16, 1", "17, 3", "18, 1", "19, 4", "20, 1",
+      "21, 5", "22, 1", "23, 6", "24, 1", "25, 7",
+      "26, 1", "27, 8", "28, 1", "29, 9", "30, 2",
+      "31, 0", "32, 2", "33, 1", "34, 2", "35, 2",
+      "36, 2", "37, 3", "38, 2", "39, 4", "40, 2",
+      //"1000, 3", "100000, 2", "100000000, 8", "1000000000, 1",
+  })
   void destruct(int input, int expected) {
     // When
     var actual = subject.destruct(input);
 
     // Then
     assertEquals(expected, actual);
-  }
-
-
-  private static Stream<Arguments> validArguments() {
-    return Stream.of(
-        arguments(1, 1),
-        arguments(2, 2),
-        arguments(9, 9),
-        arguments(10, 1),
-        arguments(15, 3)
-    );
   }
 
 }
