640c7ffa94a4a7afb2ccce073699ef6294c08317
Gleb Remniov <g.remniov@gmail.com>
2023-10-02 17:59:36 +0300

    Optimize algorithm to reduce number of iterations

--- src/main/java/org/example/FindDigitAlgorithm.java
+++ src/main/java/org/example/FindDigitAlgorithm.java
@@ -17,20 +17,33 @@ public class FindDigitAlgorithm {
     while (true) {
       step = (int) Math.floor(Math.log10(targetNumber + 1)) + 1;
 
-      if (n < step) {
-        break;
+      if (n - step * 10 >= 0) {
+        n -= step * 10;
+        targetNumber += 10;
+        continue;
       }
 
-      n -= step;
-      targetNumber++;
-    }
-
-    if (n > 0) {
-      targetNumber++;
-
-      while (n <= --step) {
-        targetNumber /= 10;
+      if (n >= step) {
+        n -= step;
+        targetNumber++;
+        continue;
       }
+
+      if (n == 0) {
+        if (targetNumber / 10 >= 1) {
+          targetNumber /= 10;
+          continue;
+        }
+      }
+
+      if (n > 0) {
+        if (n < --step) {
+          targetNumber /= 10;
+          continue;
+        }
+      }
+
+      break;
     }
 
     int result = targetNumber % 10;
